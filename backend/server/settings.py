4de43f0fd5c33576c64aea539d3118d3dd776142
29keops <robertlisita29@gmail.com>
2026-01-11 09:41:42 +0000

    Allow all hosts for Render

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -10,10 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-change-this-in-production'
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = ALLOWED_HOSTS = ["*"]
+
 
 # ======================
 # APPLICATIONS
