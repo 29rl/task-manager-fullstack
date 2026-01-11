b886cf1517873fff431708c0cc48ee042733f032
29keops <robertlisita29@gmail.com>
2026-01-11 12:05:02 +0000

    Fix requirements.txt for Render

--- backend/render_start.sh
+++ backend/render_start.sh
@@ -2,13 +2,13 @@
 set -o errexit
 
 echo "Running migrations..."
-python backend/manage.py migrate
+python manage.py migrate
 
-echo "Creating admin if not exists..."
-python backend/create_admin.py
+echo "Creating admin user..."
+python server/create_admin.py
 
-echo "Collecting static..."
-python backend/manage.py collectstatic --noinput
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
 
 echo "Starting gunicorn..."
-gunicorn server.wsgi:application
+gunicorn server.wsgi:application --bind 0.0.0.0:$PORT
